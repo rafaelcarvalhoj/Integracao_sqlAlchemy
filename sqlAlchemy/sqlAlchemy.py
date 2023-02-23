bc618939dc6612e22e7a594da2da2f8b9bb4790f
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2023-02-22 22:08:36 -0300

    Funcoes de adicionar clientes ao banco de dados

--- sqlAlchemy/sqlAlchemy.py
+++ sqlAlchemy/sqlAlchemy.py
@@ -21,7 +21,7 @@ class Cliente(Base):
 
     # atributos
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    nome = Column(String)
     cpf = Column(String(9))
     endereco = Column(String(9))
 
@@ -31,7 +31,7 @@ class Cliente(Base):
     )
 
     def __repr__(self):
-        return f"Cliente(id={self.id}, name={self.name}, cpf={self.cpf}, endereco={self.endereco})"
+        return f"Cliente(id={self.id}, name={self.nome}, cpf={self.cpf}, endereco={self.endereco})"
 
 
 class Conta(Base):
@@ -56,8 +56,38 @@ class Conta(Base):
 
 
 # criando conexao com base de dados
-
 engine = create_engine("sqlite://")
 
 # criando as classes e as tabelas dentro da base de dados
 Base.metadata.create_all(engine)
+
+# criando metodos
+
+def existe_cpf(cpf):
+    existe = select(Cliente).where(Cliente.cpf.in_(cpf))
+    for cliente in Session(engine).scalars(existe):
+        return True
+
+    return False
+
+def adicionar_conta():
+    print("=-=-=-=-= Criando uma nova conta =-=-=-=-=")
+    nome = str(input("Digite seu nome: "))
+    cpf = str(input("Digite seu CPF: "))
+    endereco = str(input("Digite seu endereco: "))
+    return Cliente(nome=nome, cpf=cpf, endereco=endereco)
+    if not existe_cpf([cpf]):
+        endereco = str(input("Digite seu endereco: "))
+        cliente = Cliente(nome=nome, cpf=cpf, endereco=endereco)
+        adicionar_conta_db(cliente)
+        return cliente
+    else:
+        print("CPF já cadastrado!\n\n")
+        return None
+
+
+def adicionar_conta_db(cliente):
+    with Session(engine) as session:
+        session.add(cliente)
+        session.commit()
+        session.close()
